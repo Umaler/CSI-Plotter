74cdf6af88997ac0596ae587dec88b06ae1d89ac
Umaler <Umaler@Umaler.Umaler>
2024-04-11 16:07:09 +0300

    Remove plplot-windows and excess files

--- GUI/MainWindow.cpp
+++ GUI/MainWindow.cpp
@@ -8,27 +8,6 @@
 
 namespace WMG {
 
-TestWindow::TestWindow() :
-    db("/home/de/DB/Exp12.01.24.sqlite"),
-    dbsource(std::move(db))
-{
-    set_default_size(100, 100);
-
-    dbsource.signalOnNewDataArrived().connect([](std::vector<std::vector<std::pair<double, double>>> data) {
-        for(const auto& i : data) {
-            for(const auto& j : i) {
-                std::cout << j.first << " " << j.second << std::endl;
-            }
-            std::cout << "------------------" << std::endl;
-        }
-        std::cout << "End of pack" << std::endl;
-    });
-    Boundaries bounds;
-    bounds.id.max = 500; bounds.id.min = 0;
-    dbsource.setBoundaries(bounds);
-    dbsource.addCollectionType("amplitude", "ffa");
-}
-
 MainWindow::MainWindow() :
     mainBox(Gtk::Orientation::VERTICAL)
 {
@@ -41,15 +20,6 @@ MainWindow::MainWindow() :
     mainBox.set_spacing(5);
     mainBox.set_expand();
 
-    mainBox.append(openDBButton);
-    openDBButton.signal_clicked().connect( sigc::mem_fun(*this, &MainWindow::onOpenGraphClicked) );
-
-    mainBox.append(openRTButton);
-    openRTButton.signal_clicked().connect( sigc::mem_fun(*this, &MainWindow::onOpenRTGraphClecked) );
-
-    mainBox.append(openTestButton);
-    openTestButton.signal_clicked().connect( sigc::mem_fun(*this, &MainWindow::onOpenTestWindow) );
-
     mainBox.append(openDSPButton);
     openDSPButton.signal_clicked().connect( sigc::mem_fun(*this, &MainWindow::onOpenDSPWindow) );
 
@@ -58,33 +28,6 @@ MainWindow::MainWindow() :
 
     mainBox.append(openDiffPhaseDB);
     openDiffPhaseDB.signal_clicked().connect( sigc::mem_fun(*this, &MainWindow::onOpenDBPDWindow) );
-}
-
-void MainWindow::onOpenGraphClicked() {
-    if(plotWindow)
-        return;
-
-    plotWindow.reset(new PlotWindow);
-    plotWindow->signal_unmap().connect( [&](){plotWindow.reset();} );
-    plotWindow->show();
-}
-
-void MainWindow::onOpenRTGraphClecked() {
-    if(rtWindow)
-        return;
-
-    rtWindow.reset(new RealTimeWindow);
-    rtWindow->signal_unmap().connect( [&](){rtWindow.reset();} );
-    rtWindow->show();
-}
-
-void MainWindow::onOpenTestWindow() {
-    if(tWindow)
-        return;
-
-    tWindow.reset(new TestWindow);
-    tWindow->signal_unmap().connect( [&](){tWindow.reset();} );
-    tWindow->show();
 }
 
 void MainWindow::onOpenDSPWindow() {
--- GUI/MainWindow.hpp
+++ GUI/MainWindow.hpp
@@ -3,23 +3,11 @@
 #include <gtkmm.h>
 
 #include <string>
-#include "PlotWindow.hpp"
-#include "RealTimeWindow.hpp"
 #include "DataSourcePlotWindow.hpp"
 #include "RealTimeWindow.hpp"
 #include "../DataSources/DBSource.hpp"
 
 namespace WMG {
-
-class TestWindow : public Gtk::ApplicationWindow {
-public:
-    TestWindow();
-
-private:
-    SQLite::Database db;
-    DBSource dbsource;
-
-};
 
 class MainWindow : public Gtk::ApplicationWindow {
 public:
@@ -27,9 +15,6 @@ public:
 
 private:
 
-    void onOpenGraphClicked();
-    void onOpenRTGraphClecked();
-    void onOpenTestWindow();
     void onOpenDSPWindow();
     void onOpenRTDSWindow();
     void onOpenDBPDWindow();
@@ -39,16 +24,10 @@ private:
 
     Gtk::Box mainBox;
 
-    Gtk::Button openDBButton{"_Print graph by data from database", true};
-    Gtk::Button openRTButton{"_Print real time graph", true};
-    Gtk::Button openTestButton{"_Test window", true};
     Gtk::Button openDSPButton{"_Open Data Source Plot Window", true};
     Gtk::Button openRTDSButton{"_Open Real Time DS Window", true};
     Gtk::Button openDiffPhaseDB{"_Open from DB with phase diff", true};
 
-    std::unique_ptr<PlotWindow> plotWindow;
-    std::unique_ptr<RealTimeWindow> rtWindow;
-    std::unique_ptr<TestWindow> tWindow;
     std::unique_ptr<DataSourcePlotWindow> dspWindow;
     std::unique_ptr<DataSourcePlotWindow> rtdsWindow;
     std::unique_ptr<DataSourcePlotWindow> dbpdWindow;
